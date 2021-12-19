da576fdb14cd81a1f0ea9eaee5671bbc708f5be7
Jasper Oudemans <jasperoudemans@gmail.com>
2021-12-19 10:39:23 -0700

    It fetches

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,26 @@
 const APIKey = "c5a2aadd2ba954ccd8c6e99df9c362c9";
-let city = "";
+let city = "chicago";
 var queryURL =
   "http://api.openweathermap.org/data/2.5/weather?q=" +
   city +
-  "&appid=" +
+  "&units=imperial&appid=" +
   APIKey;
+
+function fetchChicago() {
+  fetch(queryURL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("Didn't reach");
+        return;
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      document.getElementById("temp").textContent = data.main.temp;
+    });
+}
+
+fetchChicago();
+console.log(fetchChicago);
