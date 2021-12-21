73451efca6b83b6ec27f1aeb96fccd244bd5cb10
Jasper Oudemans <jasperoudemans@gmail.com>
2021-12-20 20:56:14 -0800

    got search function to work

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,15 @@
 const APIKey = "c5a2aadd2ba954ccd8c6e99df9c362c9";
-let city = "chicago";
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&units=imperial&appid=" +
-  APIKey;
 
-function fetchChicago() {
+document.getElementById("searchBtn").addEventListener("click", fetchCity);
+
+function fetchCity() {
+  const city = document.getElementById("searchInput").value;
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=" +
+    APIKey;
+
   fetch(queryURL)
     .then((response) => {
       if (response.ok) {
@@ -23,6 +26,3 @@ function fetchChicago() {
       document.getElementById("humidity").textContent = data.main.humidity;
     });
 }
-
-fetchChicago();
-console.log(fetchChicago);
